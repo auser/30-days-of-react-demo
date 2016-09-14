f8c6ed4a4d63d102b0e4082c482a8dd1ecd5c374
Travis CI <arilerner@mac.com>
2016-09-14 00:01:54 -0700

    Updates for deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-set -e
+set -o errexit -o nounset
 
 SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
+
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 function doCompile {
   (
@@ -11,7 +19,6 @@ function doCompile {
     git add -f build
     git commit -am "Rebuild website"
     git filter-branch -f --prune-empty --subdirectory-filter build
-    git push -f origin gh-pages
   )
 }
 
@@ -22,54 +29,21 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]
     exit 0
 fi
 
-git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
-
-
-# Save some useful information
-REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-SHA=`git rev-parse --verify HEAD`
-
-# Clone the existing gh-pages for this repo into out/
-# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO out
-cd out
-git checkout $SOURCE_BRANCH || git checkout --orphan $SOURCE_BRANCH
-cd ..
-
-# Clean out existing contents
-rm -rf out/**/* || exit 0
-
-# Run our compile script
-doCompile
-
-# Now let's go have some fun with the cloned repo
-cd out
-
-# Checkout gh-pages
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-
-# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
+if [ "$TRAVIS_BRANCH" != "master" ]
+then
+  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
+  exit 0
 fi
 
-# Commit the "changes", i.e. the new version.
-# The delta will show diffs between new and old versions.
-git add .
-git commit -m "Deploy to GitHub Pages: ${SHA}"
+rev=$(git rev-parse --short HEAD)
 
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
-chmod 600 deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
+mkdir -p out && cd $_
+git init
 
-# Now that we're all set up, we can push.
-git push $SSH_REPO $TARGET_BRANCH
+doCompile
+
+touch .
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
